d76927bb48b26d7d3728059de2a67a38f11b8b17
Alex Denisov <ad@tessonics.com>
2022-11-26 09:14:09 -0500

    adding test-env command

--- cmd/go-build/main.go
+++ cmd/go-build/main.go
@@ -4,6 +4,7 @@ import "github.com/alecthomas/kong"
 
 var cli struct {
 	DiscoverToolchains DiscoverToolchains `cmd:"" help:"Show available C/C++ toolchains."`
+	TestEnv            TestEnv            `cmd:"" help:"Detect C/C++ toolchain from environment variables."`
 	Version            kong.VersionFlag   `short:"v" help:"Print version information and quit."`
 }
 
@@ -16,68 +17,4 @@ func main() {
 	)
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
-
-	/*
-		app := cli.App("go-build", "GO build library utilitity")
-
-		app.Command("discover-toolchains", "Show available C/C++ toolchains", func(c *cli.Cmd) {
-			c.Spec = "[--verbose] [--output=<filename>] [--format=<content-format>] [--types=<type-filter>] [--installations]"
-			verbose := c.BoolOpt("v verbose", false, "show verbose output")
-			output := c.StringOpt("o output", "", "output file (uses stdout if not specified)")
-			format := c.StringOpt("f format", "summary", "output format")
-			types := c.StringOpt("t types", "", "comma separated toolchain types (msvc,clang,gcc)")
-			installations := c.BoolOpt("i installations", false, "show compiler installations instead of toolchains")
-
-			c.Action = func() {
-				buf := []byte{}
-				var err error
-				var feedback func(string)
-				if *verbose {
-					feedback = func(s string) {
-						log.Println(s)
-					}
-				}
-				if *installations {
-					ii := compiler.DiscoverInstallations(strings.Split(*types, ","), feedback)
-					if *format == "json" {
-						buf, err = json.MarshalIndent(ii, "", "  ")
-						check(err)
-					} else {
-						w := &bytes.Buffer{}
-						for _, i := range ii {
-							i.PrintSummary(w)
-						}
-						if len(ii) == 0 {
-							fmt.Fprintln(w, "no installations found")
-						}
-						buf = w.Bytes()
-					}
-				} else {
-					tt := compiler.DiscoverToolchains(true, strings.Split(*types, ","), feedback)
-					if *format == "json" {
-						buf, err = json.MarshalIndent(tt, "", "  ")
-					} else if *format == "summary" {
-						w := &bytes.Buffer{}
-						for _, tc := range tt {
-							tc.PrintSummary(w)
-						}
-						if len(tt) == 0 {
-							fmt.Fprintln(w, "no compilers found")
-						}
-						buf = w.Bytes()
-					} else {
-						err = fmt.Errorf("unsupported format '%s'", *format)
-					}
-					check(err)
-				}
-				if *output == "" {
-					fmt.Println(string(buf))
-				} else {
-					check(ioutil.WriteFile(*output, buf, 0666))
-				}
-			}
-		})
-
-		app.Run(os.Args)
-	*/
 }
--- compiler/cc/builder.go
+++ compiler/cc/builder.go
@@ -3,26 +3,35 @@ package cc
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/adnsv/go-build/compiler/clang"
+	"github.com/adnsv/go-build/compiler/gcc"
+	"github.com/adnsv/go-build/compiler/msvc"
 	"github.com/adnsv/go-build/compiler/toolchain"
 	"github.com/adnsv/go-utils/filesystem"
 )
 
 type Builder struct {
-	Tools          toolchain.Toolset `json:"tools" yaml:"tools"`
 	Compiler       string            `json:"compiler" yaml:"compiler"` // MSVC/GCC/CLANG
+	Version        string            `json:"version" yaml:"version"`
+	FullVersion    string            `json:"full-version,omitempty" yaml:"full-version,omitempty"`
+	Tools          toolchain.Toolset `json:"tools" yaml:"tools"`
 	CCIncludeDirs  []string          `json:"cc-include-dirs,omitempty" yaml:"cc-include-dirs,omitempty"`
 	CXXIncludeDirs []string          `json:"cxx-include-dirs,omitempty" yaml:"cxx-include-dirs,omitempty"`
 	LibraryDirs    []string          `json:"library-dirs,omitempty" yaml:"library-dirs,omitempty"`
-	Environment    []string          `json:"environment" yaml:"environment"`
+	Environment    []string          `json:"environment,omitempty" yaml:"environment,omitempty"`
 }
 
 func FromChain(tc toolchain.Chain) *Builder {
 	return &Builder{
 		Tools:          tc.Tools,
 		Compiler:       tc.Compiler,
+		Version:        tc.Version,
+		FullVersion:    tc.FullVersion,
 		CCIncludeDirs:  tc.CCIncludeDirs,
 		CXXIncludeDirs: tc.CXXIncludeDirs,
 		LibraryDirs:    tc.LibraryDirs,
@@ -31,12 +40,16 @@ func FromChain(tc toolchain.Chain) *Builder {
 }
 
 func FromEnv() (*Builder, error) {
-	b := &Builder{}
+	b := &Builder{
+		Tools: toolchain.Toolset{},
+	}
 	cc := os.Getenv("cc")
 	cxx := os.Getenv("cxx")
 	if cc == "" && cxx == "" {
 		return nil, errors.New("missing CC/CXX environment vars")
 	}
+
+	compilers := map[string]struct{}{} // multiple compiler detector
 
 	if cc != "" {
 		cc, err := filepath.Abs(cc)
@@ -47,6 +60,18 @@ func FromEnv() (*Builder, error) {
 			return nil, fmt.Errorf("invalid CC path: %w", err)
 		}
 		b.Tools[toolchain.CCompiler] = filepath.ToSlash(cc)
+		exe := executableStem(cc)
+		switch {
+		case exe == "cl":
+			b.Compiler = "MSVC"
+		case exe == "clang":
+			b.Compiler = "CLANG"
+		case strings.HasSuffix(exe, "gcc"):
+			b.Compiler = "GCC"
+		}
+		if b.Compiler != "" {
+			compilers[b.Compiler] = struct{}{}
+		}
 	}
 
 	if cxx != "" {
@@ -58,8 +83,90 @@ func FromEnv() (*Builder, error) {
 			return nil, fmt.Errorf("invalid CXX path: %w", err)
 		}
 		b.Tools[toolchain.CXXCompiler] = filepath.ToSlash(cxx)
-
+		exe := executableStem(cc)
+		switch {
+		case exe == "cl":
+			b.Compiler = "MSVC"
+		case exe == "clang":
+			b.Compiler = "CLANG"
+		case strings.HasSuffix(exe, "g++"):
+			b.Compiler = "GCC"
+		}
+		if b.Compiler != "" {
+			compilers[b.Compiler] = struct{}{}
+		}
 	}
 
-	return nil, nil
+	if len(compilers) > 1 {
+		return nil, fmt.Errorf("inconsistent compiler types detected")
+	}
+
+	if b.Compiler == "" || b.Compiler == "GCC" {
+		var v *gcc.Ver
+		var err error
+		if cxx != "" {
+			v, err = gcc.QueryVersion(cxx)
+		} else {
+			v, err = gcc.QueryVersion(cc)
+		}
+		if err == nil {
+			b.Compiler = "GCC"
+			b.CCIncludeDirs = v.CCIncludeDirs
+			b.CXXIncludeDirs = v.CXXIncludeDirs
+			b.Version = v.Version
+			b.FullVersion = v.FullVersion
+		}
+	}
+	if b.Compiler == "" || b.Compiler == "CLANG" {
+		var v *clang.Ver
+		var err error
+		if cxx != "" {
+			v, err = clang.QueryVersion(cxx)
+		} else {
+			v, err = clang.QueryVersion(cc)
+		}
+		if err == nil {
+			b.Compiler = "CLANG"
+			b.CCIncludeDirs = v.CCIncludeDirs
+			b.CXXIncludeDirs = v.CXXIncludeDirs
+			b.Version = v.Version
+			b.FullVersion = v.FullVersion
+		}
+	}
+	if b.Compiler == "" || b.Compiler == "MSVC" {
+		var ver string
+		//var target string
+		var err error
+		if cxx != "" {
+			ver, _, err = msvc.QueryVersion(cxx)
+		} else {
+			ver, _, err = msvc.QueryVersion(cc)
+		}
+		if err == nil {
+			b.Compiler = "MSVC"
+			b.Version = ver
+		}
+	}
+
+	if b.Compiler == "" {
+		return nil, fmt.Errorf("unsupported compiler type")
+	}
+
+	return b, nil
+}
+
+func executableStem(fn string) string {
+	fn = strings.ToLower(filepath.Base(fn))
+	ext := filepath.Ext(fn)
+	if ext == ".exe" {
+		fn = strings.TrimSuffix(fn, ext)
+	}
+	return fn
+}
+
+func (b *Builder) PrintSummary(w io.Writer) {
+	fmt.Fprintf(w, "compiler: %s %s\n", b.Compiler, b.Version)
+	if b.FullVersion != "" {
+		fmt.Fprintf(w, "- full version: '%s'\n", b.FullVersion)
+	}
 }
