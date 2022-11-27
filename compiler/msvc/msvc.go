9d1b163ca4bbe0365124aa741b65ea964125e706
Alex Denisov <alex.a.denisov@gmail.com>
2022-11-26 19:49:03 -0500

    refactored msvc version

--- compiler/msvc/msvc.go
+++ compiler/msvc/msvc.go
@@ -7,6 +7,8 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
+
+	"github.com/adnsv/go-build/compiler/toolchain"
 )
 
 // Installation corresponds to an instance of VisualStudio
@@ -46,4 +48,44 @@ func QueryVersion(exe string) (ver, target string, err error) {
 		return "", "", errors.New("unsupported version output")
 	}
 	return match[1], strings.TrimSpace(match[2]), nil
+}
+
+func Compare(c1, c2 *toolchain.Chain) int {
+	q1, e1 := ParseVersionQuad(c1.Version)
+	q2, e2 := ParseVersionQuad(c2.Version)
+	if e1 == nil && e2 == nil {
+		if i := q1.Compare(q2); i != 0 {
+			return i
+		}
+	} else if e1 == nil {
+		return -1
+	} else if e2 == nil {
+		return +1
+	}
+	q1, e1 = ParseVersionQuad(c1.WindowsSDKVersion)
+	q2, e2 = ParseVersionQuad(c1.WindowsSDKVersion)
+	if e1 == nil && e2 == nil {
+		if i := q1.Compare(q2); i != 0 {
+			return i
+		}
+	} else if e1 == nil {
+		return -1
+	} else if e2 == nil {
+		return +1
+	}
+	q1, e1 = ParseVersionQuad(c1.UCRTVersion)
+	q2, e2 = ParseVersionQuad(c1.UCRTVersion)
+	if e1 == nil && e2 == nil {
+		if i := q1.Compare(q2); i != 0 {
+			return i
+		}
+	} else if e1 == nil {
+		return -1
+	} else if e2 == nil {
+		return +1
+	}
+	if i := strings.Compare(c1.FullVersion, c2.FullVersion); i != 0 {
+		return i
+	}
+	return strings.Compare(c1.InstalledDir, c2.InstalledDir)
 }
